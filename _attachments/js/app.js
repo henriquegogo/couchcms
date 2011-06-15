fd9b257fb01875aa46c899a8f0667956575c78e6
Henrique Soares <henrique@henrique.(none)>
2011-06-15 01:12:22 -0300

    Commiting actual status. Just routes and simple login

---
+++ _attachments/js/app.js
@@ -0,0 +1,122 @@
+var db = $.couch.db("akayu");
+google.load("feeds", "1");
+
+var app = Sammy('#colRight', function() {
+  this.use('Mustache');
+  this.use('Title');
+  this.use('GoogleAnalytics');
+
+  this.setTitle('Akayu Tecnologia');
+
+  var userCtx = {};
+
+  this.helpers({
+    menuActive: function(menu) {
+      $("#menu li").removeClass("active");
+      $("#menu li." + menu).addClass("active");
+      var title = $("#menu li.active a").html();
+      this.titleCurrent(title);
+    },
+    titleCurrent: function(title) {
+      if (title) {
+        this.title(" - " + title);
+        $("#page-title").html(title).show();
+      } else {
+        this.title();
+        $("#page-title").hide();
+      }
+    }
+  });
+
+  this.around(function(callback) {
+    $.couch.session({success: function(resp) {
+      if (resp.userCtx.name != null) userCtx = resp.userCtx;
+      callback();
+    }});
+  });
+
+  this.before(function(ctx) {
+    ctx.menuActive();
+    ctx.titleCurrent();
+    if (userCtx.name) ctx.user = userCtx;
+    ctx.render('templates/user.mustache').replace("#user");
+  });
+
+  this.get('#/', function(ctx) {
+    ctx.redirect('#/projects');
+  });
+
+  this.get('#/login', function(ctx) {
+    ctx.titleCurrent('Login');
+    ctx.partial('templates/login.mustache');
+  });
+
+  this.post('#/login', function(ctx) {
+    $.couch.login({name: ctx.params.user, password: ctx.params.password, success: function(resp) {
+      ctx.redirect("#/account");
+    }, error: function() {
+      ctx.redirect("#/account");
+    }});
+  });
+
+  this.get('#/logout', function(ctx) {
+    $.couch.logout({success: function(resp) {
+      userCtx = {};
+      ctx.redirect("#/account");
+    }});
+  });
+
+  this.get('#/account', function(ctx) {
+    $.couch.session({success: function(resp) {
+      if (resp.userCtx.name != null) {
+        ctx.titleCurrent('Minha área');
+
+        ctx.partial('templates/account.mustache');
+      } else { ctx.redirect("#/login"); }
+    }});
+  });
+
+  this.get('#/home', function(ctx) {
+    ctx.partial('templates/home.mustache');
+  });
+
+  this.get('#/partners', function(ctx) {
+    ctx.menuActive('partners');
+
+    ctx.content = "Sem conteúdo";
+
+    ctx.partial('templates/page.mustache');
+  });
+
+  this.get('#/projects', function(ctx) {
+    ctx.menuActive('projects');
+
+    $.getJSON("http://github.com/api/v2/json/repos/show/henriquegogo?callback=?", function(json) {
+      ctx.projects = json.repositories.reverse();
+      ctx.partial('templates/projects.mustache');
+    });
+  });
+
+  this.get('#/events', function(ctx) {
+    ctx.menuActive('events');
+
+    var feeds = new google.feeds.Feed('http://www.google.com/calendar/feeds/akayu.com.br_8r5ujs48g7bboeq3lovipnpg28@group.calendar.google.com/public/full');
+    feeds.load(function(result) {
+      ctx.events = result.feed.entries.reverse();
+
+      ctx.partial('templates/events.mustache');
+    });
+  });
+
+  this.get('#/blog', function(ctx) {
+    ctx.menuActive('blog');
+
+    ctx.content = "Sem conteúdo";
+
+    ctx.partial('templates/page.mustache');
+  });
+});
+
+$(document).ready(function() {
+  app.run('#/');
+});
---
+++ _attachments/js/loader.js
@@ -0,0 +1,18 @@
+function loadScript(scripts) {
+  for (var i=0; i < scripts.length; i++) {
+    document.write('<script src="'+scripts[i]+'"><\/script>')
+  };
+};
+
+loadScript([
+  "js/jquery-1.4.2.min.js",
+  "js/couchdb/sha1.js",
+  "js/couchdb/json2.js",
+  "js/couchdb/jquery.couch.js",
+  "js/sammy/sammy-latest.min.js",
+  "js/sammy/sammy.mustache-latest.min.js",
+  "js/sammy/sammy.title-latest.min.js",
+  "js/sammy/sammy.googleanalytics-latest.min.js",
+  "js/fancybox/jquery.fancybox-1.3.4.pack.js",
+  "http://www.google.com/jsapi"
+]);
